b73eb7fd4624afac1795ecb5a4d4dc37541b4971
Raumpels <13harlock13@gmail.com>
2024-09-19 10:12:57 +0200

    React

---
+++ meet2trek-react/src/page/Home.jsx
@@ -0,0 +1,232 @@
+import { useState } from "react"
+
+export function Home() {
+  const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false)
+
+  const toggleRegisterPopup = () => {
+    setIsRegisterPopupOpen(!isRegisterPopupOpen)
+  }
+
+  return (  <>
+    
+
+  <main>
+      {/* Popup de Registro */}
+      {isRegisterPopupOpen && (
+        <div className="popup-overlay">
+          <div className="popup-content">
+            <span className="close-btn" onClick={toggleRegisterPopup}>&times;</span>
+            <h2>Registro</h2>
+            <form id="registrationForm" action="tu-url-de-base-de-datos" method="POST">
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="username">Nombre de usuario:</label>
+                <input type="text" id="usernSame" name="username" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Contraseña:</label>
+                <input 
+                  type="password" 
+                  id="password" 
+                  name="password" 
+                  placeholder="8 caracteres entre letras y números" 
+                  required 
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" 
+                  title="La contraseña debe tener al menos 8 caracteres, con letras y números." 
+                  style={{ fontSize: "1em" }} 
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="confirmPassword">Repetir contraseña:</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" required style={{ fontSize: "1em" }} />
+                <span id="La contraseña no coincide" style={{ color: "red", fontSize: "0.9em" }}></span>
+              </div>
+              <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {/* Popup de Inicio de Sesión */}
+      {isLoginPopupOpen && (
+        <div className="popup-overlay">
+          <div className="popup-content">
+            <span className="close-btn" onClick={toggleLoginPopup}>&times;</span>
+            <h2>Iniciar Sesión</h2>
+            <form id="loginForm" action="url-base-de-datos" method="POST">
+              <div className="form-group">
+                <label htmlFor="loginUsername">Nombre de usuario:</label>
+                <input type="text" id="loginUsername" name="loginUsername" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="loginPassword">Contraseña:</label>
+                <input type="password" id="loginPassword" name="loginPassword" required />
+              </div>
+              <button type="submit" className="btn btn-primary">Iniciar</button>
+              <p style={{ color: "black", marginTop: "10px" }}>
+                Si no estás registrado haz clic <a href="#" onClick={toggleRegisterPopup}><u>aquí</u></a>.
+              </p>
+              <p style={{ color: "black", marginTop: "10px" }}>
+                Has olvidado la contraseña? Haz clic <a href="#"><u>aquí</u></a>.
+              </p>
+            </form>
+          </div>
+        </div>
+      )}
+
+      <div className="row" style={{ marginTop: "30px" }}>
+        <div className="col-xl-3 mb-5">
+          <img src="img/past2.jpg" alt="Image" className="img-fluid" style={{ maxWidth: "100%" }} />
+        </div>
+        <div className="col-xl-3 mb-5">
+          <img src="img/past1.jpg" alt="Image" className="img-fluid" style={{ maxWidth: "100%" }} />
+        </div>
+        <div className="col-xl-3 mb-5">
+          <img src="img/past3.jpg" alt="Image" className="img-fluid" style={{ maxWidth: "100%" }} />
+        </div>
+      </div>
+
+      <div className="main-index">
+        <aside>
+          <div id="filter">
+            <h2 className="tm-text-primary">Filtros de búsqueda</h2>
+            <form id="contact-form" action="" method="POST">
+              <div className="form-group">
+                <select className="form-control" id="contact-select" name="inquiry">
+                  <option>Provincia</option>
+                  <option>Málaga</option>
+                  <option disabled>Córdoba</option>
+                  <option disabled>Sevilla</option>
+                  <option disabled>Cádiz</option>
+                  <option disabled>Huelva</option>
+                  <option disabled>Almería</option>
+                  <option disabled>Jaén</option>
+                  <option disabled>Granada</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <select className="form-control" id="contact-select" name="inquiry">
+                  <option>Población</option>
+                  <option>Málaga</option>
+                  <option>Ronda</option>
+                  <option>Antequera</option>
+                  <option>Axarquía</option>
+                  <option>Guadalhorce</option>
+                  <option>Abdalajís</option>
+                  <option>Costa del Sol</option>
+                  <option>Almijara</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <input 
+                  type="number" 
+                  name="dific" 
+                  className="form-control" 
+                  placeholder="Dificultad (del 1 al 10)" 
+                  min="0" 
+                  max="10" 
+                />
+              </div>
+              <div className="form-group">
+                <input 
+                  type="number" 
+                  name="distancia" 
+                  className="form-control" 
+                  placeholder="Distancia (de 1 a 30km)" 
+                  min="0" 
+                  max="30" 
+                />
+              </div>
+              <div className="tm-text-right" style={{ marginTop: "20px" }}>
+                <button type="submit" className="btn btn-primary">Buscar rutas</button>
+                <button type="button" className="btn btn-primary" onClick={toggleLoginPopup}>Guardar filtros</button>
+              </div>
+            </form>
+          </div>
+        </aside>
+
+        <section className="css-raul">
+          <div className="card featured-card">
+            <a href="ruta-recomend-detail.html">
+              <h1 className="titular" style={{ textAlign: "center", padding: "15px", margin: "20px 20px 0 20px" }}>
+                Ruta recomendada del mes
+              </h1>
+            </a>
+            <img className="img-destacada" src="img/Chilla/rioChilla3.jpg" alt="Imagen destacada" />
+            <div className="card-content">
+              <a href="ruta-recomend-detail.html">
+                <h2 className="tm-text-primary" style={{ marginBottom: "0px", textAlign: "center" }}>
+                  Sendero acuático río Chillar
+                </h2>
+              </a>
+              <h3 className="tm-text-primary" style={{ marginTop: "0px", textAlign: "center" }}>Nerja. Málaga</h3>
+              <p className="article-title" style={{ fontSize: "15px", textAlign: "center", padding: "0px 60px", lineHeight: "1.3rem" }}>
+                El camino del Río Chillar es una ruta muy conocida, pero el 99% de las personas se detienen en el Vado de
+                Los Patos o incluso antes. No todo el mundo sabe que con un poco más de dificultad, esfuerzo y
+                orientación,
+                dos kilómetros más arriba, en medio de una naturaleza mucho más salvaje y exuberante, se esconde un
+                pequeño
+                paraíso natural de cascadas y piscinas naturales.
+              </p>
+              <p className="card-features">Dificultad: Alta<br />Distancia: 15km</p>
+            </div>
+          </div>
+          <div className="card-columns">
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+            <div className="card">
+              <img src="https://via.placeholder.com/300x150" alt="Imagen 1" />
+              <div className="card-content">
+                <h3 className="card-title">Título 1</h3>
+                <h4>Descripción 1</h4>
+                <p className="card-features">Dificultad: Fácil<br />Distancia: 5km</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+</main>
+    </>
+    )
+}
---
+++ meet2trek-react/src/page/ProfileUser.jsx
@@ -0,0 +1,219 @@
+// {/*import { useState } from "react"
+//     const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false)
+//     const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false)
+    
+//     const toggleRegisterPopup = () => {
+//         setIsRegisterPopupOpen(!isRegisterPopupOpen)
+//         }
+        
+//         const toggleLoginPopup = () => {
+//             setIsRegisterPopupOpen(!isRegisterPopupOpen)
+//             }
+// export function ProfileUser () {
+  
+// return ( <>
+// <main>
+//     <section>
+//         <div className="container-fluid tm-mt-60 mb-4">
+//             <div className="row tm-mb-50">
+//                 <div className="col-lg-4 col-12">
+//                         <div>
+//                         <h2 className="tm-text-primary mb-4">Modificar mis datos de registro</h2>
+//                         <form id="segistrationForm" action="url-de-base-de-datos-user" method="POST">
+//                             <div className="form-group form-group1">
+//                             <label htmlFor="email" className="form-label">Email:</label>
+//                             <input type="email" id="esail" name="email" required />
+//                             </div>
+//                             <div className="form-group form-group1">
+//                             <label htmlFor="username" className="form-label">Nombre de usuario:</label>
+//                             <input type="text" id="usernsme" name="username" required />
+//                             </div>
+//                             <div className="form-group form-group1">
+//                             <label htmlFor="password" className="form-label">Antigua contraseña:</label>
+//                             <input type="password" id="psssword" name="password" placeholder="8 caracteres entre letras y números"
+//                                 required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+//                                 title="La contraseña debe tener al menos 8 caracteres, con letras y números."
+//                                 style="font-size: 0.8em" />
+//                             </div>
+//                             <div className="form-group form-group1">
+//                             <label htmlFor="password" className="form-label">Nueva contraseña:</label>
+//                             <input type="password" id="psssword" name="password" placeholder="8 caracteres entre letras y números"
+//                                 required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+//                                 title="La contraseña debe tener al menos 8 caracteres, con letras y números."
+//                                 style="font-size: 0.8em" />
+//                             </div>
+//                             <div className="form-group">
+//                             <label htmlFor="confirmPassword" className="form-label">Repetir contraseña:</label>
+//                             <input type="password" id="confirmPsssword" name="confirmPassword" required style="font-size: 1em" />
+//                             <span id="La contraseña no coincide" style="color: red; font-size: 0.9em"></span>
+//                             </div>
+//                         </form>
+//                         {isRegisterPopupOpen && (
+//                         <button type="submit" className="btn btn-primary">Enviar</button>
+//                         )}
+//                     </div>
+//                 </div>
+
+//                 <div className="col-lg-4 col-12 mb-4 mb-5">
+//                     <img src="img/people-3.jpg" alt="Image" className="mb-4 img-fluid" style="border-radius: 50%; padding-bottom: 0sx" />
+//                     <h2 className="tm-text-primary" style="margin-bottom: 0.5rem">
+//                     Fran Fersández
+//                     </h2>
+//                     <h3 className="tm-text-secondary h5" style="margin-bottom: 0.5rem">
+//                     Amante ds la naturaleza
+//                     </h3>
+//                     <p className="mb-4">
+//                     Mauris snte tellus, feugiat nec metus non, bibendum semper velit.
+//                     Praesent laoreet urna id tristique fermentum. Morbi venenatis dui
+//                     quis diam mollis pellentesque.
+//                     </p>
+//                 </div>
+
+//                 <div className="col-lg-4 col-12 mb-4">
+//                     <h2 className="tm-text-primary mb-4">Modificar mi perfil</h2>
+//                     <form id="segistrationForm" action="url-de-base-de-datos-user" method="POST">
+//                         <div className="form-group form-group1 frase">
+//                             <label htmlFor="frase" className="form-label">Frase que me define</label>
+//                             <input type="text" id="frsse" className="form-control" maxLength="30" placeholder="Escribe tu frase" />
+//                         </div>
+//                         <div className="form-group perfil">
+//                             <label htmlFor="perfil" className="form-label">Mi perfil</label>
+//                             <textarea id="perfil" className="form-control" rows="7" maxLength="200" placeholder="Describete coso quieras"></textarea>
+//                         </div>
+//                     </form>
+//                     {isRegisterPopupOpen && (
+//                     <button style="float: right" type="submit" className="btn btn-primary"> Enviar </button>
+//                     )}
+//                 </div>
+//             </div>
+
+//                 <div className="cont-rutas">
+//                     <h1 className="rutas-titular">Mis quedadas</h1>
+//                         <div className="tabla">
+//                             <table className="tg">
+//                             <thead>
+//                                 <tr>
+//                                 <th>Provincia</th>
+//                                 <th>Localidad</th>
+//                                 <th>Fecha y hora</th>
+//                                 <th>Rutas guardadas</th>
+
+//                                 </tr>
+//                             </thead>
+
+//                             <tbody>
+//                                 <tr>
+//                                 <td className="tg-0pky">Málaga</td>
+//                                 <td className="stg-0pky">Nerja</td>
+//                                 <td className="stg-0pky">22/09/2024 a la 09h.</td>
+//                                 <td className="stg-0pky"><a href="ruta-recomend-detail.html"><u>Sendero acuático río Chillar</u></a></td>
+
+//                                 </tr>
+//                                 <tr>
+//                                 <td className="tg-0pky">Málaga</td>
+//                                 <td className="stg-0pky">Ardales</td>
+//                                 <td className="stg-0pky">10/10/2024 a la 08h.</td>
+//                                 <td className="stg-0pky"><a href="ruta-detail.html"><u>Caminito del Rey</u></a></td>
+//                                 </tr>
+//                                 <tr>
+//                                 <td className="tg-0pky">Málaga</td>
+//                                 <td className="stg-0pky">Antequera</td>
+//                                 <td className="stg-0pky">24/10/2024 a la 08,30h.</td>
+//                                 <td className="stg-0pky"><a href="#"><u>Sendero del Torcal</u></a></td>
+
+//                                 </tr>
+//                                 {/*<!-- Repetir lo mismo para las demás filas -->*/}
+//                             </tbody>
+//                             </table>
+
+//                             <h1 className="rutas-titular" style="margin-top: 50px;">Rutas guardadas</h1>
+//                             <div className="tabla">
+//                                 <table className="tg">
+//                                     <thead>
+//                                         <tr>
+//                                             <th>Provincia</th>
+//                                             <th>Localidad</th>
+//                                             <th>Ruta</th>
+//                                             <th>Quedadas</th>
+//                                             <th>Eliminar</th>
+//                                         </tr>
+//                                     </thead>
+
+//                                     <tbody>
+//                                         <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Nerja</td>
+//                                             <td className="stg-0pky"><a href="ruta-recomend-detail.html"><u>Sendero acuático río Chillar</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo1" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;" href="quedadas.html"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar1" style="text-align: center; color: red; cursor: pointer;">
+//                                             <u>Elimisar</u>
+//                                             </td>
+
+//                                         </tr>
+//                                         <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Ardales</td>
+//                                             <td className="stg-0pky"><a href="ruta-detail.html"><u>Caminito del Rey</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo2" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar2" style="text-align: center; color: red; cursor: pointer;">
+//                                             <u>Elimisar</u>
+//                                             </td>
+
+//                                         </tr>
+//                                         <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Antequera</td>
+//                                             <td className="stg-0pky"><a href="user-rute1-detail.html"><u>Sendero del Torcal</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo3" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar3" style="text-align: center; color: red; cursor: pointer;">
+//                                             <u>Elimisar</u>
+//                                             </td>
+
+//                                         </tr>
+//                                         <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Nerja</td>
+//                                             <td className="stg-0pky"><a href="ruta-recomend-detail.html"><u>Sendero acuático río Chillar</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo1" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;" href="quedadas.html"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar4" style="text-align: center; color: red; cursor: pointer;">
+//                                             <u>Elimisar</u>
+//                                             </td>
+
+//                                         </tr>
+//                                         <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Ardales</td>
+//                                             <td className="stg-0pky"><a href="ruta-detail.html"><u>Caminito del Rey</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo2" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar5" style="text-align: center; color: red; cursor: pointer;">
+//                                             <u>Elimisar</u>
+//                                             </td>
+
+//                                         </tr>
+//                                             <tr>
+//                                             <td className="tg-0pky">Málaga</td>
+//                                             <td className="stg-0pky">Antequera</td>
+//                                             <td className="stg-0pky"><a href="user-rute1-detail.html"><u>Sendero del Torcal</u></a></td>
+//                                             <td className="stg-0pky" id="formarGrupo3" style="text-align: center; cursor: pointer;"><a
+//                                                 style="scolor: green;"><u>Ver quedadas en esta ruta</u></a></td>
+//                                             <td className="tg-0pky" id="eliminar6" style="text-align: center; color: red;"><u>Eliminar</u></td>
+
+//                                             </tr>
+//                                     </tbody>
+//                                 </table>
+//                             </div>
+//                         </div>
+//                 </div>
+//             <button style="float: right; margin-top: 30px;" type="submit" className="btn btn-primary" onclick="confirmarCierreSesion()">Cerrar sesión
+//             </button>
+//         </div>
+//     </section>
+// </main>
+// </> )
+// }
+// */}
