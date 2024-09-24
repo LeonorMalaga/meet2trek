0a778401fff18efa41539ec1900f8d20e3730311
LeonorMalaga <leonormartinezmesas@gmail.com>
2024-09-24 05:35:58 +0200

    add user data backup

---
+++ meet2trek_user_data_only.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for macos14 (arm64)
+--
+-- Host: 127.0.0.1    Database: meet2trek
+-- ------------------------------------------------------
+-- Server version	8.4.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'strong young man','Jonathan@hotmail.com','../USERS/Jonathan0/icon/people-4.jpg','Jonathan','1234asdf','ARDALES','SPAIN',5,8000,'MALAGA','I am the best','Hein','Jonathan0'),(2,'Joven dinamico y emprendedor, siempre dispuesto a ayudar','Antonio@gmail.com','../USERS/Antonio0/icon/people-1.jpg','Antonio','1234asdf','ARDALES','SPAIN',3,6000,'MALAGA','Una sonrisa ante todo','Jimenez','Antonio0'),(3,'Joven entusiasta de la naturaleza a la que le gusta descubrir nuevos rincones.','Claudia@gmail.com','../USERS/Clauida0/icon/people-2.jpg','Claudia','1234asdf','FRIGILIANA','SPAIN',6,10000,'MALAGA','Aire, viento y sol es todo lo que necesito','Gonzalez','Claudia0'),(4,'Sonriente y amigable, siempre dispuesto a hechar un buen rato de chachara. 25 años , soñador y amante de los animales.','Alberto@gmail.com','../USERS/Alberto0/icon/Alberto.jpg','Alberto','1234asdf','FRIGILIANA','SPAIN',1,2000,'MALAGA','Mirando siempre al frente sin perder el objetivo','Gonzalez','Alberto0');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-24  5:34:03
